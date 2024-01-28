ad8e576bc867858fd08cd02371f5d95aac7f82d0
giffordk <giffordk@AAD-PF4E4RSQ>
2024-01-28 16:07:52 -0600

    checking for EXTI12 in EXTI15_10_IRQHandler

--- CPE3300Project1/Src/interrupts.c
+++ CPE3300Project1/Src/interrupts.c
@@ -11,6 +11,21 @@ static volatile SYSCFG* const syscfg = (SYSCFG*) SYSCFG_ADR;
 static volatile EXTI* const exti = (EXTI*) EXTI_ADR;
 static volatile RCC* const rcc = (RCC*) RCC_ADR;
 
+typedef enum {
+	 IDLE,
+	 BUSY,
+	 COLLISION
+} STATE;
+
+static STATE state = IDLE;
+
+/**
+ * edge_detection_init:
+ * Enables rising and falling edge detection for pin PC12
+ * and resulting interrupt call on EXTI12.
+ * parameters: none
+ * returns: none
+ */
 void edge_detection_init(void) {
 	// enable RCC for SYSCFG
 	rcc->APB2RENR |= (1<<14);
@@ -18,18 +33,40 @@ void edge_detection_init(void) {
 	// external interrupt configuration for PC12
 	syscfg->EXTICR4 |= 0x0010;
 
-	// unmask EXTI line 12
+	// enable EXTI line 12
 	exti->IMR |= (1<<12);
 
-	// enable rising and falling edge detection for EXTI line 12
+	// enable rising and falling edge detection for EXTI12
 	exti->RTSR |= (1<<12);
 	exti->FTSR |= (1<<12);
 
 	NVIC_EnableIRQ(EXTI15_10n);
 }
 
+/**
+ * EXTI15_10_IRQHandler:
+ * Interrupt handler for EXTI12 resulting from edge detection from pin PC12
+ * parameters: none
+ * returns: none
+ */
 void EXTI15_10_IRQHandler(void) {
-	NVIC_ClearPendingIRQ(EXTI15_10n);
-	//TODO figure out how to differentiate that this IRQ is from EXTI12
+	//verify interrupt was called by EXTI12
+	if (exti->PR & (1<<12)) {
+		//TODO begin counter to detect idle or collision
+		switch (state) {
+		case IDLE: {
 
+			break;
+			}
+		case BUSY: {
+
+			break;
+			}
+		case COLLISION: {
+
+			break;
+			}
+		};
+	}
+	NVIC_ClearPendingIRQ(EXTI15_10n);
 }
